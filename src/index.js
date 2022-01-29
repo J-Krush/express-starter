aba85ff5b023c0b7f78833525344ec973e14b577
John Kreisher <jkreish@gmail.com>
2022-01-29 12:43:17 -0500

    feat: calculate astrology from origin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const { Origin, Horoscope } = require("circular-natal-horoscope-js")
 
 // Create express app
 const app = express()
-const bodyParser = require('body-parser')
 const port = 5000
 
 // Middleware
@@ -11,6 +12,29 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/elemental-code', async function (req, res) {
     console.log('elemental code endpoint: ', req.body)
+
+    const origin = new Origin({
+        year: 1989,
+        month: 5, // 0 = January, 11 = December!
+        date: 29,
+        hour: 14,
+        minute: 32,
+        latitude: 45.7833,
+        longitude: -108.5007,
+    })
+
+    const horoscope = new Horoscope({
+        origin: origin,
+        houseSystem: "whole-sign",
+        zodiac: "sidereal",
+        aspectPoints: ['bodies', 'points', 'angles'],
+        aspectWithPoints: ['bodies', 'points', 'angles'],
+        aspectTypes: ["major", "minor"],
+        customOrbs: {},
+        language: 'en'
+    })
+
+    console.log(horoscope.CelestialBodies)
     
     res.sendStatus(200)
 })
