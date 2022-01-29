3199c004fd1062a7687b3c24ed5bcff3de91dac1
John Kreisher <jkreish@gmail.com>
2022-01-29 15:23:54 -0500

    chore: add signs and elements to constants

---
+++ src/constants/astrological-constants.js
@@ -0,0 +1,24 @@
+
+const elements = {
+    earth: 'earth', 
+    water: 'water',
+    fire: 'fire',
+    mineral: 'mineral',
+    air: 'air',
+    nature: 'nature'
+}
+const signElement = {
+    aries: elements.fire,
+    taurus: elements.earth,
+    gemini: elements.air,
+    cancer: elements.water,
+    leo: elements.fire,
+    virgo: elements.earth,
+    libra: elements.air,
+    scorpio: elements.water,
+    sagittarius: elements.fire,
+    capricorn: elements.earth,
+    aquarius: elements.air,
+    pisces: elements.water,
+    ophiuchus: 'Ophiuchus', // TODO: ophiuchus element?
+}
