34d3b528a5363950daf51832d3d1c962538e678e
John Kreisher <jkreish@gmail.com>
2022-02-20 12:11:51 -0500

    feat: generate pdf from chart html file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { Origin, Horoscope } = require('circular-natal-horoscope-js')
-// const astrology = require('./helpers/astrochart')
 // TODO: Add rate limiter: https://www.npmjs.com/package/express-rate-limit
+
+// PDF
+const { generatePDF } = require('./helpers/generatePDF')
 
 // Constants
 const { signElement } = require('./constants/astrologicalConstants')
@@ -92,6 +94,8 @@ app.post('/chart', async function (req, res) {
         console.log(item.key, ': ', item.ChartPosition.Ecliptic.DecimalDegrees)
     })
 
+
+
     // for (body in horoscope.CelestialBodies.all) {
     //     console.log(body)
     // }
@@ -99,69 +103,11 @@ app.post('/chart', async function (req, res) {
 
     // console.log(horoscope.Angles)
 
-    // var page = require('webpage').create();
-    // page.open('http://github.com/', function() {
-    //     page.render('github.png');
-    //     phantom.exit();
-    // });
+    const pdf = await generatePDF()
 
-    // res.sendFile(page)
+    console.log('pdf: ', pdf)
 
-    // var radix = new astrology.Chart('paper', 600, 600).radix( horoscope );
-
-    const { document } = new JSDOM(`
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="utf-8">
-            <title>Radix</title>  
-            <style>
-                #paper{
-                    background:#999;
-                }
-            </style>                   
-        </head>
-        <body>    	    
-            <div id="paper"></div>
-                                        
-            <script src="helpers/astrochart.min.js"></script>
-            <script src="html2canvas.js"></script>
-            <script type="text/javascript"> 
-            
-                var data = {
-                    "planets":{"Lilith":[18], "Chiron":[18], "Pluto":[63], "Neptune":[110, 0.2], "Uranus":[318], "Saturn":[201, -0.2], "Jupiter":[192], "Mars":[210], "Moon":[268], "Sun":[281], "Mercury":[312], "Venus":[330], "NNode":[2]},
-                    "cusps":[296, 350, 30, 56, 75, 94, 116, 170, 210, 236, 255, 274]			
-                };
-                                                        
-                window.onload = function () {            	     
-                    var radix = new astrology.Chart('paper', 600, 600).radix( data );
-                                    
-                    // Aspect calculation
-                    // default is planet to planet, but it is possible add some important points:				
-                    radix.addPointsOfInterest( {"As":[data.cusps[0]],"Ic":[data.cusps[3]],"Ds":[data.cusps[6]],"Mc":[data.cusps[9]]});				
-                    radix.aspects();
-                    
-                    // window.scrollTo(0, 0);
- 
-                    // Convert the div to image (canvas)
-                    html2canvas(document.getElementById("paper")).then(function (canvas) {
-                
-                        // Get the image data as JPEG and 0.9 quality (0.0 - 1.0)
-                        console.log(canvas.toDataURL("image/jpeg", 0.9));
-                    });
-                };
-            </script>		    
-        </body>
-    </html>
-    `, { runScripts: "dangerously", resources: "usable" }).window
-
-    
-
-    console.log(document)
-
-    
-
-    // res.sendFile(window.document.body)
+    // res.send(pdf)
 
     res.sendStatus(200)
 })
