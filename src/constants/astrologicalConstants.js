2fab4e038007ed47707a749d9f326ef1fc48f007
John Kreisher <jkreish@gmail.com>
2022-03-23 21:20:24 -0400

    feat: add nasa command codes for horizons api calls

--- src/constants/astrologicalConstants.js
+++ src/constants/astrologicalConstants.js
@@ -23,23 +23,24 @@ const signElement = {
     ophiuchus: 'Ophiuchus', // TODO: what is ophiuchus element?
 }
 const celestialBodies = {
-    sun: "Sun",
-	moon: "Moon",
-	mercury: "Mercury",
-	venus: "Venus",
-	mars: "Mars",
-	jupiter: "Jupiter",
-	saturn: "Saturn",
-	uranus: "Uranus",
-	neptune: "Neptune",
-	pluto: "Pluto",
-	chiron: "Chiron",
-	lilith: "Lilith",
-	nnode: "NNode",
+    sun: {title: 'Sun', code: 10},
+	moon: {title: 'Moon', code: 301},
+	mercury: {title: 'Mercury', code: 199},
+	venus: {title: 'Venus', code: 299},
+    earth: {title: 'Earth', code: 399},
+	mars: {title: 'Mars', code: 499},
+	jupiter: {title: 'Jupiter', code: 599},
+	saturn: {title: 'Saturn', code: 699},
+	uranus: {title: 'Uranus', code: 799},
+	neptune: {title: 'Neptune', code: 899},
+	pluto: {title: 'Pluto', code: 999},
+	chiron: {title: 'Chiron', code: 'DES=2002060;'},
+	lilith: {title: 'Lilith', code: 'DES=2001181;'},
 }
 
 
 module.exports = {
     elements,
-    signElement
+    signElement,
+    celestialBodies
 }
