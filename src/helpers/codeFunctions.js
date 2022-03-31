9eeaa769ed57ff8e89cd97cb1c5814b3a3b64aa6
John Kreisher <jkreish@gmail.com>
2022-03-30 20:01:32 -0400

    refactor: code logic, mineral and nature rules, display rules

--- src/helpers/codeFunctions.js
+++ src/helpers/codeFunctions.js
@@ -1,8 +1,8 @@
 const { elements, zodiacSigns } = require('../constants/astrologicalConstants')
-const { linearCode } = require('./utilities')
+const { linearCode, getLastDigitOfYear, elementCountInArray } = require('./utilities')
 
 const dagaraElement = (year) => {
-    var lastDigitYear = Number.isInteger(year) ? year % 10 : parseInt(year.toString().substr(-1))
+    var lastDigitYear = getLastDigitOfYear(year)
 
     // TODO revisit this logic
     var northNodeExperience
@@ -33,7 +33,7 @@ const dagaraElement = (year) => {
     }
 }
 
-const fullElementalCode = (year, sunSign, nNode, sNode) => {
+const fullElementalCode = (year, sunSign, northNode, southNode) => {
     // 1 (Indigenious): Dagara year
     // 2 (Achievement): Sidereal sun sign
     // 3 (Soul Path): South node
@@ -44,59 +44,104 @@ const fullElementalCode = (year, sunSign, nNode, sNode) => {
     console.log('full elemental code')
     console.log('year: ', year)
     console.log('sunSign: ', sunSign)
-    console.log('nNode: ', nNode)
-    console.log('sNode: ', sNode)
+    console.log('northNode: ', northNode)
+    console.log('southNode: ', southNode)
 
-    const indigeniousElement = dagaraElement(year)
+    const indigenious = { element: dagaraElement(year) }
 
-    const sunSignElement = zodiacSigns[sunSign.sign].element
-    const southNodeElement = zodiacSigns[sNode.sign].element
-    const legacyElement = '?'
-    const northNodeElement = zodiacSigns[nNode.sign].element
-    const galacticElement = '?'
+    // TODO: Legacy and Galactic
+    const legacyElement = { element: '?' }
+    const galacticElement = { element: '?' }
 
-    const rawArray = [indigeniousElement, sunSignElement, southNodeElement, legacyElement, northNodeElement, galacticElement]
+    const rawArray = [indigenious, sunSign, southNode, legacyElement, northNode, galacticElement]
 
     console.log('rawArray: ', rawArray)
 
+    // Build filtered code array.
+    // Start with Indigenious (Dagara Year).
+    var codeArray = [indigenious]
 
-    // Build filtered code array. Start with indigenious.
-    var codeArray = [indigeniousElement]
-
-    // Achievement
-    if (sunSignElement !== indigeniousElement) {
-        codeArray.push(sunSignElement)
+    // Achievement (Sun Sign)
+    if (sunSign.element !== indigenious.element && sunSign.cusp === false) {
+        codeArray.push(sunSign)
     } else {
-        codeArray.push(elements.mineral)
+        const mineralCount = elementCountInArray(codeArray, elements.mineral)
+        if (mineralCount === 0) {
+            codeArray.push({
+                ...sunSign,
+                element: elements.mineral
+            })
+        } else {
+            codeArray.push({
+                ...sunSign,
+                element: elements.nature
+            })
+        }
     }
 
-    // Soul Path
-    if (southNodeElement !== indigeniousElement && southNodeElement !== sunSignElement) {
-        codeArray.push(southNodeElement)
-    } else if (southNodeElement === indigeniousElement && southNodeElement === sunSignElement) {
-        codeArray.push(elements.nature)
-    }  else if (southNodeElement === indigeniousElement || southNodeElement === sunSignElement) {
-        codeArray.push(elements.mineral)
+    // Soul Path (South Node)
+    if (southNode.element !== indigenious.element && southNode.element !== sunSign.element && southNode.cusp === false) {
+        codeArray.push(southNode)
+    } else if (southNode.element === indigenious.element && southNode.element === sunSign.element) {
+        codeArray.push({
+            ...southNode,
+            element: elements.nature
+        })
+    }  else  {
+        const mineralCount = elementCountInArray(codeArray, elements.mineral)
+        const natureCount = elementCountInArray(codeArray, elements.nature)
+        if (mineralCount === 0) {
+            codeArray.push({
+                ...southNode,
+                element: elements.mineral
+            })
+        } else if (mineralCount !== 0 && natureCount !== 0) {
+            codeArray.push(southNode)
+        } else {
+            codeArray.push({
+                ...southNode,
+                element: elements.nature
+            })
+        }
     }
 
     // Legacy: TODO
     codeArray.push(legacyElement)
 
     // Multidimensional
-    const duplicates = rawArray.slice(0, 3).filter(item => item === northNodeElement).length
-    if (duplicates === 0) {
-        codeArray.push(northNodeElement)
-    } else if (duplicates === 1 && codeArray.indexOf(elements.mineral) !== -1) {
-        codeArray.push(elements.nature)
-    } else if (duplicates === 1 && codeArray.indexOf(elements.mineral) === -1) {
-        codeArray.push(elements.mineral)
+    if (northNode.element !== indigenious.element && northNode.element !== sunSign.element && northNode.element !== southNode.element && northNode.cusp === false) {
+        codeArray.push(northNode)
+    } else if (northNode.element === indigenious.element && northNode.element === sunSign.element) {
+        codeArray.push({
+            ...northNode,
+            element: elements.nature
+        })
+    }  else {
+        const mineralCount = elementCountInArray(codeArray, elements.mineral)
+        const natureCount = elementCountInArray(codeArray, elements.nature)
+        if (mineralCount === 0) {
+            codeArray.push({
+                ...northNode,
+                element: elements.mineral
+            })
+        } else if (mineralCount !== 0 && natureCount !== 0) {
+            codeArray.push(northNode)
+        } else {
+            codeArray.push({
+                ...northNode,
+                element: elements.nature
+            })
+        }
     }
 
     // Galactic: TODO
     codeArray.push(galacticElement)
 
+    console.log('final code array: ', codeArray)
 
-    return linearCode(codeArray)
+    // console.log('raw array: ', linearCode(rawArray))
+    // console.log('filtered array: ', linearCode(codeArray, year))
+    return `${linearCode(codeArray, year)} (${linearCode(rawArray, 0, true)})`
 }
 
 module.exports = {
--- src/helpers/utilities.js
+++ src/helpers/utilities.js
@@ -1,3 +1,4 @@
+const { zodiacSigns } = require('../constants/astrologicalConstants')
 
 const checkForDegreeOverflow = (deg) => {
     if (deg < 360 && deg >= 0) {
@@ -13,17 +14,44 @@ const firstChar = (string) => {
     return string.charAt(0).toUpperCase()
 }
 
-const linearCode = (codeArray) => {
-    return `${firstChar(codeArray[0])}-${firstChar(codeArray[1])}-${firstChar(codeArray[2])}-${firstChar(codeArray[3])}-${firstChar(codeArray[4])} --- ${firstChar(codeArray[0])}-${firstChar(codeArray[1])}-${firstChar(codeArray[4])}-${firstChar(codeArray[3])}-${firstChar(codeArray[2])}`
+const displayCusp = (signAndDegree) => {
+    return `${firstChar(signAndDegree.element)}${signAndDegree.cusp && signAndDegree.cusp !== false ? '/' + firstChar(zodiacSigns[signAndDegree.cusp.zodiac].element) : ''}`
+}
+
+const linearCode = (codeArray, year = 0, displayCusps = false) => {
+    var lastDigitYear
+    if (year !== 0) { lastDigitYear = getLastDigitOfYear(year) }
+    
+    const firstElement = firstChar(codeArray[0].element)
+    const secondElement =  displayCusps ? displayCusp(codeArray[1]) : firstChar(codeArray[1].element)
+    const thirdElement = displayCusps ? displayCusp(codeArray[2]) : firstChar(codeArray[2].element)
+    const fourthElement = firstChar(codeArray[3].element)
+    const fifthElement = displayCusps ? displayCusp(codeArray[4]) : firstChar(codeArray[4].element)
+    
+    return `${firstElement}-${secondElement}-${thirdElement}-${fourthElement}-${fifthElement} // ${firstElement}-${secondElement}-${fifthElement}-${fourthElement}-${thirdElement} ${year !== 0 ? `*${lastDigitYear}` : ''}`
 }
 
 const getTwoDecimalNumber = (number) => {
     return Math.round(number * 100) / 100
 }
 
+const getLastDigitOfYear = (year) => {
+    return Number.isInteger(year) ? year % 10 : parseInt(year.toString().substr(-1))
+}
+
+const elementCountInArray = (array, element) => {
+    var count = 0
+    array.forEach((sign, index) => {
+        if (sign.element === element) ++count
+    })
+    return count
+}
+
 module.exports = {
     checkForDegreeOverflow,
     firstChar,
     linearCode,
-    getTwoDecimalNumber
+    getTwoDecimalNumber,
+    getLastDigitOfYear,
+    elementCountInArray
 }
